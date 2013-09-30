f628236844dcc1eb620d1b26d556ae9902c333bf
padminisitaraman <padmini.vittal@sella.it>
2013-09-30 16:34:08 +0530

    Take it
    Test it

---
+++ test1e10_problem/test1e10/About.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test1e10
+{
+    public partial class About : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["cook"] != null)
+            {
+                Response.Write("No Cookie");
+                Response.End();
+            }
+            else
+            {
+                Response.Write("cookie value:" + Request.Cookies["cook"]);
+                Response.End();
+            }
+        }
+    }
+}
---
+++ test1e10_problem/test1e10/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test1e10
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("cook", "chicken");
+            cookie.Secure = true;
+            Response.Cookies.Add(cookie);
+            Response.Redirect("~/About.aspx");
+        }
+    }
+}
